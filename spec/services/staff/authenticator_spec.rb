94b8d8528bde6b034a351ec790fd31155765cc11
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-23 21:14:52 +0900

    スタッフログインのテスト

---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+
+describe Staff::Authenticator do
+  describe "#authenticate" do
+    example "正しいパスワードならtrueを返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+
+    example "誤ったパスワードならfalseを返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+
+    example "正しいパスワードならtrueを返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+
+    example "正しいパスワードならtrueを返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+
+    example "正しいパスワードならtrueを返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+  end
+end

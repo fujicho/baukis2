683f0748aab64a7e4b5a18ff717a877491606ce2
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-23 21:14:39 +0900

    staffのログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,23 @@ class ApplicationController < ActionController::Base
 
   include ErrorHandlers if Rails.env.production?
 
-  private def set_layout
+  private
+  
+  def set_layout
     if params[:controller].match(%r{\A(staff|admin|cutomer)/})
       Regexp.last_match[1]
     else
       "customer" 
     end   
   end
+
+
+  def current_staff_member
+    if session[:staff_member_id]
+      @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
+    end
+  end
+
+  helper_method :current_staff_member
+
 end

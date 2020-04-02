465c4059e12a759b44d9024691d99d970269241b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-02 09:56:36 +0900

    強制ログアウトの実装

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,6 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
 
   private def current_administrator
     if session[:administrator_id]
@@ -14,6 +15,14 @@ class Admin::Base < ApplicationController
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
       redirect_to :admin_login
+    end
+  end
+
+  private def check_account
+    if current_administrator && current_administrator.suspended?
+      session.delete(:administrator_id)
+      flash.alert
+      redirect_to :admin_root
     end
   end
 end

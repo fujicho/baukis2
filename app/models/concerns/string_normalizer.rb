1c781d45a230ddaad2ab8ec0d7578deb4b4c8756
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-05 12:31:43 +0900

    Create string_normalizer.rb

---
+++ app/models/concerns/string_normalizer.rb
@@ -0,0 +1,16 @@
+require "nkf"
+
+module StringNormalizer
+  extend ActiveSupport::Concern
+
+  def normalize_as_name(text)
+    NKF.nkf("-W -w -z1",text).strip if text
+  end
+
+  def normalize_as_furigana(text)
+    NKF.nkf("-W -w -z1 --katakana",text).strip if text
+  end
+end
+
+
+

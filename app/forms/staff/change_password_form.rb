89c409e7f0e126b2d4bf182c79bbf337d3f5e430
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-06 09:26:16 +0900

    修正

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -4,9 +4,8 @@ class Staff::ChangePasswordForm
   attr_accessor :object, :current_password, :new_password,
     :new_password_confirmation
 
-    def save
-      object.password = new_password
-      object.save!
-    end
+  def save
+    object.password = new_password
+    object.save!
   end
 end

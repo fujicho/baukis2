bac6ac013040b8730074ef448b1f847e42e2e9a8
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-09 23:46:54 +0900

    軽微な修正

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -39,12 +39,13 @@ class FormPresenter
       m << text_field(name, options)
       m << error_messages_for(name)
     end
+    
   end
 
   def error_messages_for(name)
     markup do |m|
       object.errors.full_messages_for(name).each do |message|
-        m.div(class: "errors-message") do |m|
+        m.div(class: "error-message") do |m|
           m.text message
         end
       end

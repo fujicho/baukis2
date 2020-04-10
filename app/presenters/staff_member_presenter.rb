c01be81feb1307c1752ad7ac8f4814f2302cb6d9
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-10 09:45:16 +0900

    氏名の記述へのプレゼンターを適用

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,10 +1,19 @@
 class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
 
+  def full_name
+    object.family_name + " " + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + " " + object.given_name_kana
+  end
+  
   # 職員の停止フラグのON/OFFを表現する記号を返す。
   # ON: BALLOT BOX WITH CHECK (U+2611)
   # OFF: BALLOT BOX (U+2610)
   def suspended_mark
     suspended? ? raw("&#x2611;") : view_context.raw("&#x2610;")
   end
+
 end

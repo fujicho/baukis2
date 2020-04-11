fd93737a5829301cd7a5575bc3a93911cf0abb53
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-11 17:42:27 +0900

    Update staff_member_form_presenter.rb

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,7 +1,8 @@
-
-def suspended_check_box
-  markup(:div, class: "check-boxes") do |m|
-    m << check_box(:suspended)
-    m << label(:suspended, "アカウント停止")
+class StaffMemberFormPresenter < UserFormPresenter
+  def suspended_check_box
+    markup(:div, class: "check-boxes") do |m|
+      m << check_box(:suspended)
+      m << label(:suspended, "アカウント停止")
+    end
   end
 end

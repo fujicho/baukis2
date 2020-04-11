32a6e0529c24d8196dc40e2223c3600c5a30888d
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-11 11:51:51 +0900

    修正

--- db/migrate/20200410005807_create_addresses.rb
+++ db/migrate/20200410005807_create_addresses.rb
@@ -7,9 +7,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :prefecture, null: false
       t.string :city, null: false
       t.string :address1, null: false
-      t.string :addresses2, null: false
-      t.string :company_name, null: false
-      t.string :division_name, null: false
+      t.string :address2, null: false
+      t.string :company_name, null: false, default: ""
+      t.string :division_name, null: false, default: ""
 
       t.timestamps
     end

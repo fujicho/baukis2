0ec0f400c762282b01658af08fe284330715313d
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-15 19:28:38 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,14 @@
 class Address < ApplicationRecord
+  include StringNormalizer
+
   belongs_to :customer
+
+  before_validation do
+    self.postal_code = normalize_as_postal_code(postal_code)
+    self.city = normalize_as_name(city)
+    self.address1 = normalize_as_name(address1)
+    self.address2 = normalize_as_name(address2)
+  end
 
   PREFECTURE_NAMES = %w(
     北海道
@@ -12,5 +21,9 @@ class Address < ApplicationRecord
     福岡県 佐賀県 長崎県 熊本県宮城県 鹿児島県
     沖縄県
     日本国外
-  )
-end
+    )
+  end
+
+  validates :postal_code, format: { with: /\A\d{7}\z/, allow_blank: true }
+  validates :prefecture, inclusion: {in: PREFECTURE_NAMES, allow_blank: true}
+

f77bbb4970bb3b1d0f4304c9915af548d524a9e7
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-16 20:45:28 +0900

    Update personal_name_holder.rb

--- app/models/concerns/personal_name_holder.rb
+++ app/models/concerns/personal_name_holder.rb
@@ -1,10 +1,10 @@
 module PersonalNameHolder
   extend ActiveSupport::Concern
 
+  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}\p{alpha}]+\z/
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
-  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/
 
-  include do
+  included do
     include StringNormalizer
 
     before_validation do
@@ -15,8 +15,8 @@ module PersonalNameHolder
     end
 
     validates :family_name, :given_name, presence: true,
-  format: { with: HUMAN_NAME_REGEXP, allow_blank:true}
-  validates :family_name_kana, :given_name_kana, presence: true,
-    format: { with: KATAKANA_REGEXP, allow_blank:true}
+      format: { with: HUMAN_NAME_REGEXP, allow_blank:true}
+    validates :family_name_kana, :given_name_kana, presence: true,
+      format: { with: KATAKANA_REGEXP, allow_blank:true}
   end
 end

cd47459be4ca3ae6c771001b3ccc91b8b7c3297b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-23 22:32:40 +0900

    カスタマーの検索フォーム

---
+++ app/forms/staff/customer_serch_form.rb
@@ -0,0 +1,7 @@
+class Staff::CustomerSerchForm
+  include ActiveModel::Model
+
+  attr_accessor: :family_name_kana, :given_name_kana,
+    :birth_year, :birth_month, :birth_mday,
+    :address_type, :prefecture, :city, :phone_number
+end

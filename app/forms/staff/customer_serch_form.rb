c29f4a9beb0d6db499041aa33a2e0aab061a45a6
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-26 09:50:05 +0900

    Delete customer_serch_form.rb

--- app/forms/staff/customer_serch_form.rb
+++
@@ -1,7 +0,0 @@
-class Staff::CustomerSerchForm
-  include ActiveModel::Model
-
-  attr_accessor: :family_name_kana, :given_name_kana,
-    :birth_year, :birth_month, :birth_mday,
-    :address_type, :prefecture, :city, :phone_number
-end

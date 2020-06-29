ca6dd003dd6a032cda4110d116e9ef07b52c8c3c
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-29 19:30:56 +0900

    Update sessions_controller.rb

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -1,2 +1,38 @@
 class Customer::SessionsController < ApplicationController
+  skip_before_action :authorize
+
+  def new
+    if current_customer
+      redirect_to :customer_root
+    else
+      @form = Customer::LoginForm.new
+      render action: "new"
+    end
+  end
+
+  def create
+    @form = Customer::LoginForm.new(login_form_params)
+    if @form.email.present?
+      customer =
+        Customer.find_by("LOWER(email) = ?", @form.email.downcase)
+    end
+    if Customer::Authenticator.new(customer).authenticate(@form.password)
+      session[:customer_id] = customer.id
+      flash.notice = "ログインしました。"
+      redirect_to :customer_root
+    else
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
+      render_action: "new"
+    end
+  end
+
+  private def login_form_params
+    params.require(:customer_login_form).permit(:email, :password)
+  end
+
+  def destroy
+    session.delete(:customer_id)
+    flash.notice = "ログアウトしました。"
+    redirect_to :customer_root
+  end
 end

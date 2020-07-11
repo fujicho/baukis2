be69ecd8a336d7d03115dcfe0545f0d501e813f0
fujicho <nnnn.sohei.fujita@gmail.com>
2020-07-11 16:08:41 +0900

    staffの関連付け

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -4,6 +4,8 @@ class StaffMember < ApplicationRecord
   include PasswordHolder
 
   has_many :events, class_name: "StaffEvent", dependent: :destroy
+  has_many :programs, foreign_key: "registrant_id",
+    dependent: :restrict_with_exception
 
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2000, 1, 1),

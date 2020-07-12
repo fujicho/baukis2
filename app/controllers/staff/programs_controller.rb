1b685f8540c8a877677482d035dd8c6c7d562979
fujicho <nnnn.sohei.fujita@gmail.com>
2020-07-12 14:09:23 +0900

    create updateアクションの記述

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -15,4 +15,31 @@ class Staff::ProgramsController < Staff::Base
     @program = Program.find(params[:id])
     @program.init_virtual_attributes
   end
+
+  def create
+    @program = Program.new
+    @program.assign_attributes(program_params)
+    @program.registrant = current_staff_member
+    if @program.save
+      flash.notice = "プログラムを登録しました。"
+      redirect_to action: "index"
+    else
+      flash.now.alert = "入力に誤りがあります。"
+      render action: "new"
+    end
+  end
+
+  def update
+    @program = Program.find(params[:id])
+    @program.assign_attributes(program_params)
+    if @program.save
+      flash.notice = "プログラムを更新しました。"
+      redirect_to action: "index"
+    else
+      flash.now.alert = "入力に誤りがあります。"
+      render action: "edit"
+    end
+  end
+
+  
 end

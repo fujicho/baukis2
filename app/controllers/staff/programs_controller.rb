14111878ba6ff37bd74cca4ea1834a472126c56d
fujicho <nnnn.sohei.fujita@gmail.com>
2020-07-12 12:37:39 +0900

    Update programs_controller.rb

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,10 +1,18 @@
 class Staff::ProgramsController < Staff::Base
   def index
-    @programs = Program.order(application_start_time: :desc)
-      .includes(:registrant).page(params[:page])
+    @programs = Program.listing.page(params[:page])
   end
 
   def show
+    @program = Program.listing.find(params[:id])
+  end
+
+  def new
+    @program = Program.new
+  end
+
+  def edit
     @program = Program.find(params[:id])
+    @program.init_virtual_attributes
   end
 end

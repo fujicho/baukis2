3fbe2fa0232c63ec46e758aadeda38e47bafa71e
fujicho <nnnn.sohei.fujita@gmail.com>
2020-07-25 16:58:26 +0900

    Update entries_form.js

--- app/javascript/staff/entries_form.js
+++ app/javascript/staff/entries_form.js
@@ -1,4 +1,4 @@
-$(document).on("turbolonks:load", () => {
+$(document).on("turbolinks:load", () => {
   $("div.button-wrapper").on("click", "button#update-entries-button", () => {
     approved = []
     not_approved = []

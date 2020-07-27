3b47755e26f26d9d4c2c02eb836755c1636c6efd
fujicho <nnnn.sohei.fujita@gmail.com>
2020-07-27 19:56:58 +0900

    修正

--- app/forms/staff/entries_form.rb
+++ app/forms/staff/entries_form.rb
@@ -1,6 +1,6 @@
 class Staff::EntriesForm
   include ActiveModel::Model
-  
+
   attr_accessor :program, :approved, :not_approved, :canceled, :not_canceled
 
   def initialize(program)
@@ -30,13 +30,13 @@ class Staff::EntriesForm
     not_canceled_entry_ids = @not_canceled.split(":").map(&:to_i)
 
     ActiveRecord::Base.transaction do
-      @program.entries.where(id: approved_entry_ids.present?)
+      @program.entries.where(id: approved_entry_ids)
         .update_all(approved: true) if approved_entry_ids.present?
-      @program.entries.where(id: not_approved_entry_ids.present?)
+      @program.entries.where(id: not_approved_entry_ids)
         .update_all(approved: false) if not_approved_entry_ids.present?
-      @program.entries.where(id: canceled_entry_ids.present?)
+      @program.entries.where(id: canceled_entry_ids)
         .update_all(canceled: true) if canceled_entry_ids.present?
-      @program.entries.where(id: not_canceled_entry_ids.present?)
+      @program.entries.where(id: not_canceled_entry_ids)
         .update_all(canceled: false) if not_canceled_entry_ids.present?
     end
   end

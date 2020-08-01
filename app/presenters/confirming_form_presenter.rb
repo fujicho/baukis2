8515fbea95e7110ca545816a8d67fcc91802eb3a
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-01 11:50:50 +0900

    confirm用formプレセンターの作成

---
+++ app/presenters/confirming_form_presenter.rb
@@ -0,0 +1,31 @@
+class ConfirmingFormPresenter
+  include HtmlBuilder
+
+  attr_reader :form_builder, :view_context
+  delegate :label, :hidden_field, :object, to: :form_builder
+
+  def initialize(form_builder, view_context)
+    @form_builder = form_builder
+    @view_context = view_context
+  end
+
+  def notes
+    ""
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      if options[:disabled]
+        m.div(object.sent(name), class: "filed-value readonly")
+      else
+        m.div(object.sent(name), class: "field-value")
+        m << hidden_field(name, options)
+      end
+    end
+  end
+
+  def method_name
+    
+  end
+end

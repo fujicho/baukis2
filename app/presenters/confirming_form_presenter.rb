cc80ab72c7af76aa277c12fb5a99e8e0b402d904
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-02 10:57:28 +0900

    date_field_bloc,drop_down_list_block,decorated_labelの記述

--- app/presenters/confirming_form_presenter.rb
+++ app/presenters/confirming_form_presenter.rb
@@ -25,7 +25,23 @@ class ConfirmingFormPresenter
     end
   end
 
-  def method_name
-    
+  def date_field_block(name, label_text, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      m.div(object.sand(name), class: "field_value")
+      m << hidden_field(name, options)
+    end
+  end
+  
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      m.div(object.sand(name), class: "field_value")
+      m << hidden_field(name, options)
+    end
+  end
+
+  def decorated_label(name, label_text)
+    label(name, label_text)
   end
 end

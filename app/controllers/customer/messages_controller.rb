6af989449e21b80d4e957986703b8f810cf1e462
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-09 11:47:00 +0900

    message_controllerにconfirmとストロングパラメータの設定

--- app/controllers/customer/messages_controller.rb
+++ app/controllers/customer/messages_controller.rb
@@ -2,4 +2,20 @@ class Customer::MessagesController < Customer::Base
   def new
     @message = CustomerMessage.new
   end
+
+  #POST
+  def confirm
+    @message = CustomerMessage.new(customer_message_params)
+    @message.customer = current_customer
+    if @message.valid?
+      render action: "confirm"
+    else
+      flash.now.alert = "入力に誤りがあります。"
+      render action: "new"
+    end
+  end
+
+  private def costomer_message_params
+    params.require(:customer_message).permit(:subject, :body)
+  end
 end

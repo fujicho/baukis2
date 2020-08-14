877445c781bc4256063f04b161adcb0f5360ab9c
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-14 22:18:21 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 table_names = %w(staff_members administrators staff_events customers
-  programs entries)
+  programs entries
+  programs messages
+)
 
 table_names.each do |table_name|
   path = Rails.root.join("db", "seeds", Rails.env, "#{table_name}.rb")

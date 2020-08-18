e066512385d7b0dfcc2c8c19f5a08d091730c122
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-18 19:29:25 +0900

    Update messages_controller.rb

--- app/controllers/staff/messages_controller.rb
+++ app/controllers/staff/messages_controller.rb
@@ -1,6 +1,10 @@
 class Staff::MessagesController < Staff::Base
   def index
     @messages = Message.not_deleted.sorted.page(params[:page])
+    if params[:tag_id]
+      @messages = @messages.joins(:message_tag_links)
+        .where("message_tag_links.tag_id" => params[:tag_id])
+    end
   end
 
   # GET

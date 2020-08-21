a56ec2c780a7c0412bcac0bd5df729a00dd7a606
fujicho <nnnn.sohei.fujita@gmail.com>
2020-08-21 12:25:59 +0900

    customerからの送信機能

--- app/controllers/customer/replies_controller.rb
+++ app/controllers/customer/replies_controller.rb
@@ -16,4 +16,28 @@ class Customer::RepliesController < Customer::Base
       render action: "new"
     end
   end
+
+  def create
+    @reply = CustomerMessage.new(customer_mesasge_params)
+    if params[:commit]
+      @reply.parent = @message
+      if @reply.save
+        flash.notice = "メッセージに回答しました。"
+        redirect_to :customer_messages
+      else
+        flash.now.alert = "入力に誤りがあります。"
+        render action: "new"
+      end
+    else
+      render action: "new"
+    end
+  end
+
+  private def prepare_message
+    @message = StaffMessage.find(params[:message_id])
+  end
+
+  private def customer_message_params
+    params.require(:customer_message).permit(:subject, :body)
+  end
 end
